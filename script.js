e01f707e97e21927240c97c3e23cbb58b42b66d8
mfsfiqri <mfsfiqr@gmail.com>
2023-11-22 08:07:57 +0700

    add to play 5 rounds

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-let playerSelection = prompt("Masukkan antara rock, paper, scissors");
-playerSelection = playerSelection.toLowerCase();
+// let playerSelection = prompt("Masukkan antara rock, paper, scissors");
+// playerSelection = playerSelection.toLowerCase();
 
-const computerSelection = getComputerChoice();
+// const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
   if (
@@ -25,4 +25,29 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Masukkan antara rock, paper, scissors");
+    playerSelection = playerSelection.toLowerCase();
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+
+    if (result.includes("You win")) {
+      playerScore++;
+    } else if (result.includes("You lose")) {
+      computerScore++;
+    }
+
+    console.log(result);
+  }
+
+  if (playerScore > computerScore) {
+    console.log("You win the game!");
+  } else if (playerScore < computerScore) {
+    console.log("You lose the game!");
+  } else {
+    console.log("It's a tie game!");
+  }
+}
+
+game();
